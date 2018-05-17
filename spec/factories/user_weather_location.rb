502b56cbbfc9d2dd97fbf1b217907b0553115823
ethunk <ethunk>
2018-05-16 21:07:34 -0400

    factories are working

--- spec/factories/user_weather_location.rb
+++ spec/factories/user_weather_location.rb
@@ -1,5 +1,9 @@
 FactoryBot.define do
   factory :user_weather_location do
+    user_id '1'
+    current_weather_id '345662'
     
+    association :user, factory: :user
+    association :current_weather, factory: :current_weather
   end
 end
