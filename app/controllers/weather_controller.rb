5b35a7ef400d85b07883d5208709d3832d009fa5
ethunk <ethunk>
2018-06-02 13:40:33 -0400

    button added

--- app/controllers/weather_controller.rb
+++ app/controllers/weather_controller.rb
@@ -2,7 +2,7 @@ class WeatherController < ApplicationController
   before_action :authenticate_user!
 
   def index
-    user = current_user
-    current_weathers = user.current_weathers
+    @user = current_user
+    @current_weathers = @user.current_weathers
   end
 end
